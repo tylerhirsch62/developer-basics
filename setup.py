ede6829c8498ea7d8bc889fd6c1ba9aa27b7f934
Tyler <tyhirsch62@gmail.com>
2023-09-15 01:38:06 -0500

    try updating version no

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'developer_basics',         # How you named your package folder (MyLib)
   packages = ['developer_basics'],   # Chose the same as "name"
-  version = '0.01',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Contains basic developer helper functions',   # Give a short description about your library
   author = 'Tyler Hirsch',                   # Type in your name
   author_email = 'tylerhirschbiz@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/tylerhirsch62/developer-basics',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/tylerhirsch62/developer-basics/archive/refs/tags/v_0.02.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/tylerhirsch62/developer-basics/archive/refs/tags/v_0.03.tar.gz',    # I explain this later on
   keywords = ['developer', 'basics', 'developer_basics'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       ],
